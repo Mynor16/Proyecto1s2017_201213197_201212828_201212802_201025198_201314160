e15cc1fbc8bcd0072258a62ed6953b088697f90c
AnaOrtizM <aniitaortiz1792@gmail.com>
2017-03-20 22:27:15 -0600

    Modificacion al 20/03/17 21:28

--- Ana Ortiz/ServidorPython/Principal.py
+++ Ana Ortiz/ServidorPython/Principal.py
@@ -11,22 +11,52 @@ app = Flask('Proyecto1_ServidorPython')
 avlT = AVL()
 mt = Matriz()
 #***********************************************#
+
+#***************** METODOS AVL *****************#
+@app.route('/insertarAVL',methods=['POST']) 
+def insertarAVL():
+	articulo = str(request.form['nombre'])
+	descripcion = str(request.form['desc'])
+	avlT.insertar(str(articulo), str(descripcion))
+	return "Articulo insertado con exito!"
+
+@app.route('/graficarAVL',methods=['POST'])
+def graficarAVL():
+	ls.graficarAVL()
+	return "Lista Graficada" 
+#***********************************************#
+
+#************ METODOS  MATRIZ  ************#
+@app.route('/insertarMatriz',methods=['POST']) 
+def insertarMatriz():
+	usuario = str(request.form['user'])
+	contraseña = str(request.form['password'])
+	nombre = str(request.form['name'])
+	mt.insertar(str(usuario), str(contraseña), str(nombre))
+	return "Usuario insertado con exito!"	
+
+@app.route('/graficarMatriz',methods=['POST']) 
+def graficarMatriz():
+	mt.graficar()
+	return "Matriz Graficada" 
+#***********************************************#
+
 #***************** METODOS AVL *****************#
 	
-avlT.insertar("libro","libro hola 123")	
-avlT.insertar("libro","libro hola 123")	
-avlT.insertar("libro","libro hola 123")	
-avlT.insertar("libro","libro hola 123")	
-avlT.insertar("libro","libro hola 123")	
-print ("Articulo ingresado con exito!")
-print ("*****************************")
-avlT.graficarAVL()
-print ("Arbol Graficado!")
+# avlT.insertar("libro","libro hola 123")	
+# avlT.insertar("libro","libro hola 123")	
+# avlT.insertar("libro","libro hola 123")	
+# avlT.insertar("libro","libro hola 123")	
+# avlT.insertar("libro","libro hola 123")	
+# print ("Articulo ingresado con exito!")
+# print ("*****************************")
+# avlT.graficarAVL()
+# print ("Arbol Graficado!")
 
-mt.insertar("aom", "a1415", "Ana", "gerencia", "alo")
-mt.graficar()
+# mt.insertar("aom", "a1415", "Ana", "gerencia", "alo")#
+#mt.graficar()
 #***********************************************#
 
 if __name__ == "__main__":
 	print("Servidor iniciado...")
-	#app.run(debug=True, host='127.0.0.1')
+	app.run(debug=True, host='127.0.0.1')
