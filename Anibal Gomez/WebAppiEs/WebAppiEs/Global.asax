66daea33df1877e799c5c5265baa077c2ec10d53
nibalmorales <gonibal15@gmail.com>
2017-03-28 23:16:57 -0600

    Nueva Api
    Nueva version de api

---
+++ Anibal Gomez/WebAppiEs/WebAppiEs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppiEs.WebApiApplication" Language="C#" %>
---
+++ Anibal Gomez/WebAppiEs/WebAppiEs/Models/Reporte.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppiEs.Models
+{
+    public class Reporte
+    {
+
+        public int ID { get; set; }
+        public String Nombre { get; set; }
+        public int Pago { get; set; }
+
+    }
+}
